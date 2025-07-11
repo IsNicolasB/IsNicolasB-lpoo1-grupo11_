1a9487a2f0a1ee954ea0e0c3e960a0a572f0e915
Juan Mateo Garzon <garzonjuanmateo@gmail.com>
2025-07-11 04:42:41 -0300

    punto 2 final, con logo en el main

--- LPOOI-GRUPO11/Vistas/FrmListadoPrestamoPorDestino.Designer.cs
+++ LPOOI-GRUPO11/Vistas/FrmListadoPrestamoPorDestino.Designer.cs
@@ -20,6 +20,7 @@
             base.Dispose(disposing);
         }
 
+       
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -33,6 +34,10 @@
             this.btnBuscar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.grdPrestamos)).BeginInit();
             this.SuspendLayout();
+            this.lblAnulados = new System.Windows.Forms.Label();
+            this.lblCancelados = new System.Windows.Forms.Label();
+            this.lblOtorgados = new System.Windows.Forms.Label();
+            this.lblPendientes = new System.Windows.Forms.Label();
             // 
             // grdPrestamos
             // 
@@ -41,6 +46,7 @@
             this.grdPrestamos.Name = "grdPrestamos";
             this.grdPrestamos.Size = new System.Drawing.Size(646, 210);
             this.grdPrestamos.TabIndex = 0;
+            this.grdPrestamos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdPrestamos_CellContentClick);
             // 
             // cmbDestinos
             // 
@@ -64,7 +70,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(670, 261);
+            this.ClientSize = new System.Drawing.Size(670, 307);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.cmbDestinos);
             this.Controls.Add(this.grdPrestamos);
@@ -73,7 +79,43 @@
             this.Load += new System.EventHandler(this.FrmListadoPrestamo_Load);
             ((System.ComponentModel.ISupportInitialize)(this.grdPrestamos)).EndInit();
             this.ResumeLayout(false);
-
+            //
+            // lblOtorgados
+            //
+            this.lblOtorgados.AutoSize = true;
+            this.lblOtorgados.Location = new System.Drawing.Point(20, 330);
+            this.lblOtorgados.Name = "lblOtorgados";
+            this.lblOtorgados.Size = new System.Drawing.Size(70, 13);
+            this.lblOtorgados.Text = "Otorgados: 0";
+            //
+            // lblPendientes
+            //
+            this.lblPendientes.AutoSize = true;
+            this.lblPendientes.Location = new System.Drawing.Point(20, 350);
+            this.lblPendientes.Name = "lblPendientes";
+            this.lblPendientes.Size = new System.Drawing.Size(75, 13);
+            this.lblPendientes.Text = "Pendientes : 0";
+            //
+            // lblCancelados
+            //
+            this.lblCancelados.AutoSize = true;
+            this.lblCancelados.Location = new System.Drawing.Point(20, 370);
+            this.lblCancelados.Name = "lblCancelados";
+            this.lblCancelados.Size = new System.Drawing.Size(79, 13);
+            this.lblCancelados.Text = "Cancelados : 0";
+            //
+            // lblAnulados
+            //
+            this.lblAnulados.AutoSize = true;
+            this.lblAnulados.Location = new System.Drawing.Point(20, 390);
+            this.lblAnulados.Name = "lblAnulados";
+            this.lblAnulados.Size = new System.Drawing.Size(67, 13);
+            this.lblAnulados.Text = "Anulados: 0";
+            //
+            this.Controls.Add(this.lblOtorgados);
+            this.Controls.Add(this.lblPendientes);
+            this.Controls.Add(this.lblCancelados);
+            this.Controls.Add(this.lblAnulados);
         }
 
         #endregion
@@ -81,7 +123,11 @@
         private System.Windows.Forms.DataGridView grdPrestamos;
         private System.Windows.Forms.ComboBox cmbDestinos;
         private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label lblOtorgados;
+        private System.Windows.Forms.Label lblPendientes;
+        private System.Windows.Forms.Label lblCancelados;
+        private System.Windows.Forms.Label lblAnulados;
 
-
+     
     }
 }
